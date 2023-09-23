c035e02138241b3f5949e7f3bbc11eaf8ff007e9
Garosan <garosanchez27@gmail.com>
2023-09-23 02:11:32 -0400

    Finish initial implementation of dex

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Dex Final Repo (Moralis Blueprint)
+# GaroSwap
+
+This is a DEX interface for the EthNY 2023 hackathon.
+
+## Tech Stack
+
+- create-react-app
+- React
+- React Router
+- Wagmi
+- EthersJS
+- Moralis SDK
+- 1inch APIs
---
+++ dex/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import Header from './components/Header';
+import Swap from './components/Swap';
+import Tokens from './components/Tokens';
+import {Routes, Route} from 'react-router-dom';
+import {useConnect, useAccount} from 'wagmi';
+import {MetaMaskConnector} from 'wagmi/connectors/metaMask';
+import background from './assets/bg_img_01.jpg';
+
+function App() {
+  const {address, isConnected} = useAccount();
+  const {connect} = useConnect({
+    connector: new MetaMaskConnector(),
+  });
+
+  return (
+    <div
+      className="App"
+      style={{backgroundImage: `url(${background})`, backgroundSize: 'cover'}}
+    >
+      <Header connect={connect} isConnected={isConnected} address={address} />
+      <div className="mainWindow">
+        <Routes>
+          <Route
+            path="/"
+            element={<Swap isConnected={isConnected} address={address} />}
+          />
+          <Route path="/tokens" element={<Tokens />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
